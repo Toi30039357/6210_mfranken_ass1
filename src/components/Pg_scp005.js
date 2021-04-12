e4a4233d905546997381bc276d8f6cf57b7bb7e2
Toi30039357 <70946273+Toi30039357@users.noreply.github.com>
2021-04-13 00:07:25 +1200

    Update Pg_scp005.js

--- src/components/Pg_scp005.js
+++ src/components/Pg_scp005.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import scpData from "../json/scpobjects.json"
 import scp_image from "../images/scp_images/scp_005.png"
+import scp_image2 from "../images/scp_images/scp_005_key.jpg"
 import Speech from "../speech/speech.js"
 import breakervid from "../images/breaker.webm";
 
@@ -32,6 +33,7 @@ class Pg_scp005 extends Component
                 {breakercode}
                 <h3>Description:</h3>
                 <p className="display-linebreak">{scpData[scpSubject].description}</p>
+                <img className="imageContain" src={scp_image2} alt={scpData[scpSubject].item}></img>
                 {breakercode}
 
                 <h3>Reference:</h3>
